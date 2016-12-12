c76375cc0a9fcec51bd10663d994a8684e7640fa
Thiago Andrade <thiagoh@gmail.com>
2016-12-12 01:36:25 -0500

    fixing minor bugs

--- Assets/_Scripts/ZombieControl.cs
+++ Assets/_Scripts/ZombieControl.cs
@@ -5,79 +5,124 @@ using UnityEngine.AI;
 
 public class ZombieControl : MonoBehaviour {
 
-	public NavMeshAgent navAgent;
-	private GameObject player;
-	private WeaponControl weaponControl;
-	private Animator zombieAnimator;
+  private static float NAV_AGENT_INITIAL_SPEED = 0.8f;
+  public NavMeshAgent navAgent;
+  private GameObject player;
+  private WeaponControl weaponControl;
+  private Animator zombieAnimator;
 
-	private float hitPoints = 100f;
-	private float fadeOutTime = 5f;
-	private float timeOfDeath = 0f;
+  private static float MAX_TIME_RUNNING = 4f;
 
 
-	// Use this for initialization
-	void Start() {
+  private bool alive;
+  private float hitPoints = 100f;
+  private float fadeOutTime = 5f;
+  private float timeOfDeath = 0f;
+  private float timeOfRunStart = 0f;
+  private bool running;
+   
+  // Use this for initialization
+  void Start() {
 
-		player = GameObject.FindGameObjectWithTag ("Player");
-		weaponControl = player.GetComponentInChildren<WeaponControl>();
+    player = GameObject.FindGameObjectWithTag("Player");
+    weaponControl = player.GetComponentInChildren<WeaponControl>();
+    zombieAnimator = GetComponent<Animator>();
+    alive = true;
+    running = false;
+    setRandomSpeed(NAV_AGENT_INITIAL_SPEED);
+  }
 
-		zombieAnimator = GetComponent<Animator> ();
-	}
+  void setRandomSpeed(float minimumSpeed) {
 
-	// Update is called once per frame
-	void Update() {
+    navAgent.speed = minimumSpeed + (Random.Range(0f, minimumSpeed * 2f));
+  }
 
-		if (weaponControl.GetThreatMultiplier () < 1.0f) {
-			navAgent.Stop ();
-			zombieAnimator.enabled = false;
-		} else {
-			navAgent.SetDestination (player.transform.position);
-			navAgent.Resume ();
-			zombieAnimator.enabled = true;
-		}
+  // Update is called once per frame
+  void Update() {
+
+    timeOfRunStart += Time.deltaTime;
+
+    if (weaponControl.GetThreatMultiplier() < 1.0f) {
+      navAgent.Stop();
+      zombieAnimator.enabled = false;
+    } else {
+
+      if (alive) {
+
+        if (Vector3.Distance(player.transform.position, transform.position) < 10f) {
+          running = true;
+          timeOfRunStart = 0f;
+        }
+
+        if (running) {
+          zombieAnimator.speed = 3f;
+          setRandomSpeed(NAV_AGENT_INITIAL_SPEED * 2f);
+
+        } else {
+          zombieAnimator.speed = 1f;
+          setRandomSpeed(NAV_AGENT_INITIAL_SPEED);
+        }
+
+        navAgent.SetDestination(player.transform.position);
+        navAgent.Resume();
+        zombieAnimator.enabled = true;
+      } else {
+        navAgent.Stop();
+      }
+    }
 //		if (hitPoints > 0) {
 //			navAgent.SetDestination (player.transform.position);
 //		} else {
 //			FadeOut ();
 //		}
 
+    if (timeOfRunStart > MAX_TIME_RUNNING) {
+      running = false;
+    }
 
+    print(" running " + (running ? " true" : " false") + ". State: " + zombieAnimator.GetInteger("state"));
+  }
 
-	}
+  // Called when hit by a shot
+  void HitByBullet(object[] damageInfo) {
+    if (hitPoints > 0) {
+      string bodyPart = (string)damageInfo[0];
+      float damage = (float)damageInfo[1];
 
-	// Called when hit by a shot
-	void HitByBullet(object [] damageInfo) {
-		if (hitPoints > 0) {
-			string bodyPart = (string)damageInfo [0];
-			float damage = (float)damageInfo [1];
+      if (bodyPart.Contains("Head")) {
+        hitPoints -= damage * 4f;
 
-			if (bodyPart.Contains ("Head")) {
-				hitPoints -= damage * 2f;
-			} else if (bodyPart.Contains ("Spine")) {
-				hitPoints -= damage;
-			} else {
-				hitPoints -= damage * 0.5f;
-			}
+      } else if (bodyPart.Contains("Spine")) {
+        hitPoints -= damage;
+      } else {
+        hitPoints -= damage * 0.5f;
+      }
 
-			if (hitPoints <= 0) {
-				zombieAnimator.SetInteger ("state", 10);
-				timeOfDeath = Time.time;
-				navAgent.Stop ();
-				Destroy (gameObject, 5f);
-			}
-		}
-	}
+      if (hitPoints <= 0) {
+        die();
+      }
+    }
+  }
 
-//	void FadeOut() {
-//		float alpha = (fadeOutTime - (Time.time - timeOfDeath)) / fadeOutTime;
-//		if (alpha > 0) {
-//			foreach (MeshRenderer child in this.GetComponentsInChildren<MeshRenderer>()) {
-//				Color matCol = child.material.color;
-//				child.material.SetColor ("_Color", new Color (matCol.r, matCol.g, matCol.b, alpha));
-//			}
-//
-//		} else {
-//			Destroy (this);
-//		}
-//	}
+  void die() {
+
+    int curState = 10 + (int)(Random.Range(0f, 3f) % 3);
+    zombieAnimator.SetInteger("state", curState);
+    timeOfDeath = Time.time;
+    alive = false;
+    Destroy(gameObject, 5f);
+  }
+
+  //	void FadeOut() {
+  //		float alpha = (fadeOutTime - (Time.time - timeOfDeath)) / fadeOutTime;
+  //		if (alpha > 0) {
+  //			foreach (MeshRenderer child in this.GetComponentsInChildren<MeshRenderer>()) {
+  //				Color matCol = child.material.color;
+  //				child.material.SetColor ("_Color", new Color (matCol.r, matCol.g, matCol.b, alpha));
+  //			}
+  //
+  //		} else {
+  //			Destroy (this);
+  //		}
+  //	}
 }
